47860f61a09446f57282f14af78d15c415035050
Igwe Chinonso <igwechinonso77@gmail.com>
2023-09-28 14:01:20 +0100

    add sentry for error logging

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import { API } from "./routes";
 import { CustomError } from "./exceptions/customError";
 import { globalErrorHandler } from "./middlewares/errorHandler";
@@ -8,13 +10,28 @@ import { database } from "./config/database";
 dotenv.config();
 
 const app = express();
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
 
+app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 database.connect(app);
 
+app.use(Sentry.Handlers.tracingHandler());
 app.use("/api/v1", API.connect());
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   const error: any = new CustomError(
     `Can't find ${req.originalUrl} on this server`,
@@ -22,6 +39,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   );
   next(error);
 });
-
+app.use(Sentry.Handlers.errorHandler());
 app.use(globalErrorHandler);
 export default app;
